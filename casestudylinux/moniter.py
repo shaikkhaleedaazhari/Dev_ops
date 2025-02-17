172bdcdcd5012e99c79a92d91aa424d2f2014dc1
shaikkhaleedaazhari <129606192+shaikkhaleedaazhari@users.noreply.github.com>
2025-02-17 08:37:46 +0530

    Update moniter.py

--- casestudylinux/moniter.py
+++ casestudylinux/moniter.py
@@ -1,4 +1,4 @@
-import psutil
+import psutil  #Used for system monitoring (CPU usage, process information).
 import time
 from email_utils import get_email_credentials, send_email
 from process_utils import is_system_process, restart_application
@@ -9,12 +9,12 @@ def monitor_cpu(sender_email, receiver_email, plain_password):
         print("CPU Usage Monitoring Started")
 
         while True:
-            cpu_usage = psutil.cpu_percent(interval=1)
+            cpu_usage = psutil.cpu_percent(interval=1)  #Measures CPU usage over 1 second.
             print(f"CPU Usage: {cpu_usage}%")
 
             if cpu_usage > 60:
-                top_process = max(psutil.process_iter(["pid", "name", "cpu_percent"]),
-                                  key=lambda p: p.info["cpu_percent"])
+                top_process = max(psutil.process_iter(["pid", "name", "cpu_percent"]),  #iterates through all running process
+                                  key=lambda p: p.info["cpu_percent"])   #find the process process with highest cpu usgae
 
                 pid, name, cpu = top_process.info["pid"], top_process.info["name"], top_process.info["cpu_percent"]
 
